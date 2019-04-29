772e7ee124cfff225b47e66544bae1141c9929ed
Sadaqat Ali <36379399+sadaqatali41@users.noreply.github.com>
2019-04-29 16:17:19 +0530

    content added

--- User Management System/about.php
+++ User Management System/about.php
@@ -1,1 +1,6 @@
-<?php include('') ?>
+<?php include('includes/header.php') ?>
+<div class="container">
+    <h1>About Us</h1>
+  <h4>Coming soon...</h4>
+</div>
+<?php include('includes/footer.php') ?>
