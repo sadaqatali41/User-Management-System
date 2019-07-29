5517f338613f4ccf9b905414d796277ec81a6a6c
Sadaqat Ali <sadaqatali890@gmail.com>
2019-07-29 16:35:16 +0530

    all files are deleted successfully

--- User Management System/about.php
+++
@@ -1,6 +0,0 @@
-<?php include('includes/header.php') ?>
-<div class="container">
-    <h1>About Us</h1>
-  <h4>Coming soon...</h4>
-</div>
-<?php include('includes/footer.php') ?>
--- User Management System/contact.php
+++
@@ -1,6 +0,0 @@
-<?php include('includes/header.php') ?>
-<div class="container">
-    <h1>Contact Us</h1>
-  <h4>Coming soon...</h4>
-</div>
-<?php include('includes/footer.php') ?>
